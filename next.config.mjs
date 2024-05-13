7b07ddd0af910bb2314e66f7113b40daf495225d
WilderDev <williamjohnwilder@outlook.com>
2024-05-13 15:43:28 -0500

    CHORE: Remove plaiceholder

--- next.config.mjs
+++ next.config.mjs
@@ -1,23 +1,10 @@
-//@ts-check
-import withPlaiceholder from '@plaiceholder/next';
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
     domains: ['images.unsplash.com', 'source.unsplash.com'],
-    // remotePatterns: [
-    //   {
-    //     hostname: 'images.unsplash.com',
-    //     pathname: '/.*',
-    //   },
-    //   {
-    //     hostname: 'source.unsplash.com',
-    //     pathname: '/.*',
-    //   },
-    // ],
-  }, // https://nextjs.org/docs/pages/api-reference/next-config-js/images
+  },
   reactStrictMode: true,
   poweredByHeader: false,
 };
 
-export default withPlaiceholder(nextConfig);
+export default nextConfig;
